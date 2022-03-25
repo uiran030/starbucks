9916e69b65de42c6cf24fd500016f3ac9eba1098
jeong_euiran <90823391+uiran030@users.noreply.github.com>
2022-03-25 16:49:04 +0900

    20220325

--- js/index.js
+++ js/index.js
@@ -1,26 +1,3 @@
-// var depth1Li = document.querySelectorAll('.company-info .depth1 > li')
-// for (let i in depth1Li){
-//     depth1Li[i].firstElementChild.addEventListener('click', function(e){
-//         e.preventDefault()
-//         // this.nextElementSibling.style.display = "block"
-//         // var disp = this.nextElementSibling.style.display
-//         // if (disp !== "block"){
-//         //     this.nextElementSibling.style.display = "block"
-//         // } else {
-//         //     this.nextElementSibling.style.display = "none"
-//         // }
-
-//          밑에 방식으로 바꿔서 만들 수 있음.
-//         // depth1Li[i].className = "on"
-//         var licname = depth1Li[i].className
-//         if (licname !== "on"){
-//             depth1Li[i].className = "on"
-//         } else {
-//             depth1Li[i].className = ""
-//         }
-//     })
-// }
-
 var lia = $('.company-info .depth1 > li > a')
 lia.on('click', function(e){
     e.preventDefault()
